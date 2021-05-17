4509f5a82a547d15a26bfebde1faf7dc96838d2a
pqthinh <phamquangquang2008@gmail.com>
2021-05-18 06:44:06 +0700

    up file config db

---
+++ app/config/db.config.js
@@ -0,0 +1,16 @@
+// remotemysql
+const dbrm = {
+    HOST: "remotemysql.com",
+    USER: "ZduVtEX3Nd",
+    PASSWORD: "bleVGxRMN2",
+    DB: "ZduVtEX3Nd"
+}
+// database localhost
+const db = {
+    HOST: "192.168.101.103",
+    USER: "thinhpq",
+    PASSWORD: "",
+    DB: "fchotot"
+}
+
+module.exports = db
